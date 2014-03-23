9e7771b74877251eac04156a553c79675eaee512
miladecode <miladecode@yahoo.com>
2014-03-23 13:30:24 +1000

    Net Tools Source
    Beta source code

---
+++ NetworkTools/NetworkTools/Main.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace NetworkTools
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.lookupListView = new System.Windows.Forms.ListView();
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lookupButton = new System.Windows.Forms.Button();
+            this.lookupHostTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.resultsListView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.pingButton = new System.Windows.Forms.Button();
+            this.hostTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.tabControl1.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.ItemSize = new System.Drawing.Size(133, 23);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(402, 224);
+            this.tabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.lookupListView);
+            this.tabPage3.Controls.Add(this.lookupButton);
+            this.tabPage3.Controls.Add(this.lookupHostTextBox);
+            this.tabPage3.Controls.Add(this.label2);
+            this.tabPage3.Location = new System.Drawing.Point(4, 27);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(394, 193);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "DNS Lookup";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // lookupListView
+            // 
+            this.lookupListView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lookupListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader6});
+            this.lookupListView.Location = new System.Drawing.Point(6, 34);
+            this.lookupListView.Name = "lookupListView";
+            this.lookupListView.Size = new System.Drawing.Size(382, 153);
+            this.lookupListView.TabIndex = 3;
+            this.lookupListView.UseCompatibleStateImageBehavior = false;
+            this.lookupListView.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "IP Addresses";
+            this.columnHeader6.Width = 350;
+            // 
+            // lookupButton
+            // 
+            this.lookupButton.Location = new System.Drawing.Point(311, 5);
+            this.lookupButton.Name = "lookupButton";
+            this.lookupButton.Size = new System.Drawing.Size(75, 23);
+            this.lookupButton.TabIndex = 2;
+            this.lookupButton.Text = "Lookup";
+            this.lookupButton.UseVisualStyleBackColor = true;
+            this.lookupButton.Click += new System.EventHandler(this.lookupButton_Click);
+            // 
+            // lookupHostTextBox
+            // 
+            this.lookupHostTextBox.Location = new System.Drawing.Point(76, 7);
+            this.lookupHostTextBox.Name = "lookupHostTextBox";
+            this.lookupHostTextBox.Size = new System.Drawing.Size(229, 20);
+            this.lookupHostTextBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Host Name:";
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.resultsListView);
+            this.tabPage1.Controls.Add(this.pingButton);
+            this.tabPage1.Controls.Add(this.hostTextBox);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 27);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(394, 193);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Ping";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // resultsListView
+            // 
+            this.resultsListView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.resultsListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.resultsListView.FullRowSelect = true;
+            this.resultsListView.Location = new System.Drawing.Point(6, 34);
+            this.resultsListView.Name = "resultsListView";
+            this.resultsListView.Size = new System.Drawing.Size(382, 158);
+            this.resultsListView.TabIndex = 7;
+            this.resultsListView.UseCompatibleStateImageBehavior = false;
+            this.resultsListView.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "IP Address";
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Bytes";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 50;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Time";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 50;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "TTL";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader4.Width = 50;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Status";
+            this.columnHeader5.Width = 100;
+            // 
+            // pingButton
+            // 
+            this.pingButton.Location = new System.Drawing.Point(313, 5);
+            this.pingButton.Name = "pingButton";
+            this.pingButton.Size = new System.Drawing.Size(75, 23);
+            this.pingButton.TabIndex = 6;
+            this.pingButton.Text = "Ping";
+            this.pingButton.UseVisualStyleBackColor = true;
+            this.pingButton.Click += new System.EventHandler(this.pingButton_Click);
+            // 
+            // hostTextBox
+            // 
+            this.hostTextBox.Location = new System.Drawing.Point(76, 7);
+            this.hostTextBox.Name = "hostTextBox";
+            this.hostTextBox.Size = new System.Drawing.Size(231, 20);
+            this.hostTextBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Host Name:";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.listView1);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 27);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(394, 193);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Reverse";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader7});
+            this.listView1.Location = new System.Drawing.Point(6, 36);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(382, 153);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Domain(s)";
+            this.columnHeader7.Width = 350;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(313, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Reverse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(77, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(229, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Host Name:";
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.Title = "Save file as ...";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 224);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Net Tools Beta";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ListView resultsListView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Button pingButton;
+        private System.Windows.Forms.TextBox hostTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView lookupListView;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.Button lookupButton;
+        private System.Windows.Forms.TextBox lookupHostTextBox;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+
+    }
+}
+
---
+++ NetworkTools/NetworkTools/Main.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.Net.Sockets;
+using Newtonsoft.Json.Linq;
+
+namespace NetworkTools
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void pingButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int c = 3;
+                IPAddress ipAddress = Dns.GetHostEntry(hostTextBox.Text).AddressList[0];
+                resultsListView.Items.Clear();
+                for (int i = 0; i < c; i++)
+                {
+                    var ping = new Ping();
+                    var pingReply = ping.Send(ipAddress);
+                    var result = new ListViewItem(pingReply.Address.ToString());
+                    result.SubItems.Add(pingReply.Buffer.Count().ToString());
+                    result.SubItems.Add(pingReply.RoundtripTime.ToString());
+                    result.SubItems.Add(pingReply.Options.Ttl.ToString());
+                    result.SubItems.Add(pingReply.Status.ToString());
+                    resultsListView.Items.Add(result);
+                    System.Threading.Thread.Sleep(100);
+                }
+            }
+            catch (SocketException)
+            {
+                MessageBox.Show("Could not resolve host name.");
+            }
+            catch (PingException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (ArgumentNullException)
+            {
+                MessageBox.Show("Please enter the host name or IP address to ping.");
+            }
+            hostTextBox.Focus();
+        }
+
+        private void lookupButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                IPHostEntry hostEntry = Dns.GetHostEntry(lookupHostTextBox.Text);
+                lookupListView.Items.Clear();
+                foreach (IPAddress ipAddress in hostEntry.AddressList)
+                lookupListView.Items.Add(ipAddress.ToString());
+            }
+            catch (ArgumentNullException)
+            {
+                MessageBox.Show("Please enter the host name or IP address to ping.");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                MessageBox.Show("The host name should be less than 127 characters");
+            }
+            catch (SocketException)
+            {
+                MessageBox.Show("Could not resolve host name.");
+            }
+            hostTextBox.Focus();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var URL = "http://domains.yougetsignal.com/domains.php?remoteAddress=" + textBox1.Text;
+                using (var webClient = new WebClient())
+                {
+                    var json = webClient.DownloadString(URL);
+                    JObject o = JObject.Parse(json);
+                    var original = Convert.ToString(o["domainArray"]);
+                    var extra = new[] { "\"", "[", "]"};
+                    foreach (var rem in extra)
+                    {
+                        original = original.Replace(rem, "");
+                    }
+                    string[] parts = original.Split(',');
+                    foreach (string part in parts)
+                    {
+                        listView1.Items.Add(part);
+                    }
+                   }
+            }
+            catch (ArgumentNullException)
+            {
+                MessageBox.Show("Please enter the host name or IP address to ping.");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                MessageBox.Show("The host name should be less than 127 characters");
+            }
+            catch (SocketException)
+            {
+                MessageBox.Show("Could not resolve host name.");
+            }
+            hostTextBox.Focus();
+        }
+    }
+}
